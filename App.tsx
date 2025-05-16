be6b5b442ccd77bbcef74b3e28f17557d5bdca70
mr-Arturio <mr.arturio@gmail.com>
2025-05-15 23:48:19 -0400

    create App file, babel config etc

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-gray-800">Playground Scout</Text>
+    </View>
+  );
+}
