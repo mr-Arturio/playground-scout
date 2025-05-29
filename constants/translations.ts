306906ea69bb4a95051d7f0f4f86daff468e0fe6
mr-Arturio <mr.arturio@gmail.com>
2025-05-29 13:11:29 -0400

    translation filws

---
+++ constants/translations.ts
@@ -0,0 +1,12 @@
+export const translations = {
+  en: {
+    appTitle: "Playground Scout",
+    findPlaygrounds: "Find Playgrounds",
+    welcome: "Playground Scout",
+  },
+  fr: {
+    appTitle: "Playground Scout",
+    findPlaygrounds: "Trouver des aires de jeux",
+    welcome: "Scout des aires de jeux",
+  },
+};
